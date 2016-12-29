7192b19f03eb670b1162632e252fe9fd52dcbfd4
NewFreeCode <15813307192@139.com>
2016-12-29 17:37:46 +0800

    1.0.1 create by 12-29

--- js/common.js
+++ js/common.js
@@ -1,37 +1,17 @@
-/*
- * DOM选取
- */
-//获取
-//通过id获取元素节点;
-function getId(id) { return document.getElementById(id);
-}
-//通过类名获取类数组:
-function getClass(className) {
-	return document.getElementsByClassName(className);
-}
-//通过tagName获取类数组:
-function getTagName(parent, tagName) {
-	return parent.getElementsByTagName(tagName);
-}
-//创建DOM节点:
-function crtDom(tagName) {
-	return document.createElement(tagName);
-}
-
 //封装随机颜色
 function randomColr() {
-	var str = '0123456789abcdef';
-	var color = "#";
-	for (var i = 0; i < 6; i++) {
-		var idx = parseInt(Math.random() * str.length);
-		color += str[idx];
-	}
-	return color;
+    var str = '0123456789abcdef';
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+        var idx = parseInt(Math.random() * str.length);
+        color += str[idx];
+    }
+    return color;
 }
 
 //封装随机数
-function randomNum(min,max){
-	return parseInt(Math.random()*(max - min + 1) + min);
+function randomNum(min, max) {
+    return parseInt(Math.random() * (max - min + 1) + min);
 }
 
 
@@ -48,15 +28,15 @@ function randomNum(min,max){
 */
 
 function bind(ele, type, handler, capture) {
-	//标准
-	if (ele.addEventListener) {
-		ele.addEventListener(type, handler, capture);
-	} else if (ele.attachEvent) {
-		//ie8-
-		ele.attachEvent('on' + type, handler);
-	} else {
-		ele['on' + type] = handler;
-	}
+    //标准
+    if (ele.addEventListener) {
+        ele.addEventListener(type, handler, capture);
+    } else if (ele.attachEvent) {
+        //ie8-
+        ele.attachEvent('on' + type, handler);
+    } else {
+        ele['on' + type] = handler;
+    }
 
 }
 
@@ -68,35 +48,35 @@ function bind(ele, type, handler, capture) {
  *
  */
 function setCookie(name, val, expires, path) {
-	var cookie = name + '=' + val;
-	//如果有有效期
-	if (expires) {
-		cookie += ';expires=' + expires;
-	}
-	//如果有路径
-	if (path) {
-		cookie += ';path=' + path;
-	}
-	document.cookie = cookie;
+    var cookie = name + '=' + val;
+    //如果有有效期
+    if (expires) {
+        cookie += ';expires=' + expires;
+    }
+    //如果有路径
+    if (path) {
+        cookie += ';path=' + path;
+    }
+    document.cookie = cookie;
 }
 //getCookie('carlist');
 function getCookie(name) {
-	var cookies = document.cookie.split('; ');
-	var res = '';
-	for (var i = 0; i < cookies.length; i++) {
-		var arr = cookies[i].split('=');
-		if (arr[0] === name) {
-			res = arr[1];
-			break;
-		}
-	}
-	return res;
+    var cookies = document.cookie.split('; ');
+    var res = '';
+    for (var i = 0; i < cookies.length; i++) {
+        var arr = cookies[i].split('=');
+        if (arr[0] === name) {
+            res = arr[1];
+            break;
+        }
+    }
+    return res;
 }
 // removeCookie('carlist')
 function removeCookie(name) {
-	var now = new Date();
-	now.setDate(now.getDate() - 7);
-	setCookie(name, 'xx', now);
+    var now = new Date();
+    now.setDate(now.getDate() - 7);
+    setCookie(name, 'xx', now);
 }
 
 /*
@@ -135,18 +115,18 @@ function removeCookie(name) {
  *
  */
 function getStyle(ele, attr) {
-	//标准浏览器判断
-	if (window.getComputedStyle) {
-		return getComputedStyle(ele)[attr]
-	}
-	//IE8-
-	else if (ele.currentStyle) {
-		return ele.currentStyle[attr];
-	}
-	//都不符合条件
-	else {
-		return ele.style[attr];
-	}
+    //标准浏览器判断
+    if (window.getComputedStyle) {
+        return getComputedStyle(ele)[attr];
+    }
+    //IE8-
+    else if (ele.currentStyle) {
+        return ele.currentStyle[attr];
+    }
+    //都不符合条件
+    else {
+        return ele.style[attr];
+    }
 }
 
 /*
@@ -191,57 +171,57 @@ function getStyle(ele, attr) {
 //  = 适用无缝滚动、等动画 =
 //  ==========
 function animate(ele, opt, callback) {
-	//给ele添加timerLen属性,用于记录定时器的个数
-	ele.timerLen = 0;
+    //给ele添加timerLen属性,用于记录定时器的个数
+    ele.timerLen = 0;
 
-	//遍历属性,为每一个属性创建一个定时器
-	for (var attr in opt) {
-		createTimer(attr);
-	}
+    //遍历属性,为每一个属性创建一个定时器
+    for (var attr in opt) {
+        createTimer(attr);
+    }
 
-	//创建定时器函数
-	function createTimer(attr) {
-		//定时器的名字(标识)
-		var timerName = attr + 'timer';
+    //创建定时器函数
+    function createTimer(attr) {
+        //定时器的名字(标识)
+        var timerName = attr + 'timer';
 
-		ele.timerLen++;
-		//目标值
-		var target = opt[attr];
+        ele.timerLen++;
+        //目标值
+        var target = opt[attr];
 
-		clearInterval(ele[timerName]);
-		ele[timerName] = setInterval(function() {
-			var current = getStyle(ele, attr);
+        clearInterval(ele[timerName]);
+        ele[timerName] = setInterval(function() {
+            var current = getStyle(ele, attr);
 
-			//单位
-			var unit = current.match(/[a-z]*$/i)[0];
-			current = parseFloat(current);
+            //单位
+            var unit = current.match(/[a-z]*$/i)[0];
+            current = parseFloat(current);
 
-			//计算速度
-			var speed = (target - current) / 10;
-			if (attr === 'opacity') {
-				speed = speed.toFixed(2) * 1;
-			} else {
-				speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
-				unit = 'px';
-			}
+            //计算速度
+            var speed = (target - current) / 10;
+            if (attr === 'opacity') {
+                speed = speed.toFixed(2) * 1;
+            } else {
+                speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
+                unit = 'px';
+            }
 
-			//达到目标值
-			if (current === target || speed == 0) {
-				clearInterval(ele[timerName]);
-				current = target - speed;
+            //达到目标值
+            if (current === target || speed === 0) {
+                clearInterval(ele[timerName]);
+                current = target - speed;
 
-				//清除一个定时器就减一次
-				ele.timerLen--;
+                //清除一个定时器就减一次
+                ele.timerLen--;
 
-				//所有定时器执行完成后
-				if (ele.timerLen === 0 && typeof callback === 'function') {
-					callback();
-					console.log(ele.timerLen);
-				}
-			}
-			ele.style[attr] = current + speed + unit;
-		}, 50)
-	}
+                //所有定时器执行完成后
+                if (ele.timerLen === 0 && typeof callback === 'function') {
+                    callback();
+                    console.log(ele.timerLen);
+                }
+            }
+            ele.style[attr] = current + speed + unit;
+        }, 50);
+    }
 }
 
 /**
@@ -258,92 +238,95 @@ console.log('是否使用了该js');
  */
 function ajax(opt) {
 
-	//默认值
-	var defaults = {
-		type:"get",
-		async:true
-	}
-	//如果需要传入type,async
-	for (var attr in opt){
-		defaults[attr] = opt[attr];
-	}
-	//重新赋值给opt
-	opt = defaults;
+    //默认值
+    var defaults = {
+        type: "get",
+        async: true
+    };
+    //如果需要传入type,async
+    for (var attr in opt) {
+        defaults[attr] = opt[attr];
+    }
+    //重新赋值给opt
+    opt = defaults;
 
-	//处理参数
-	//opt.data{name:'zhangsan',age:18}
+    //处理参数
+    //opt.data{name:'zhangsan',age:18}
 
-	if (opt.data) {
-		//判断url有没有"?"
-		if(opt.url.indexOf('?') == -1){
-			opt.url += "?";
-		}else{
-			opt.url += "&";
-		}
-		//处理opt.data
-		for (var key in opt.data){
-			opt.url += key + "=" + opt.data[key] + "&";
-		}
-		//处理多个参数时最后面的&或?
-		opt.url = opt.url.replace(/[&\?]$/,"");
-	}
-	//进入JOSNP
-	if(opt.type === 'jsonp'){
-		// 2.创建script标签
-		var script = document.createElement('script');
+    if (opt.data) {
+        //判断url有没有"?"
+        if (opt.url.indexOf('?') == -1) {
+            opt.url += "?";
+        } else {
+            opt.url += "&";
+        }
+        //处理opt.data
+        for (var key in opt.data) {
+            opt.url += key + "=" + opt.data[key] + "&";
+        }
+        //处理多个参数时最后面的&或?
+        opt.url = opt.url.replace(/[&\?]$/, "");
+    }
+    //进入JOSNP
+    if (opt.type === 'jsonp') {
+        // 2.创建script标签
+        var script = document.createElement('script');
 
-		// 1.声明一个全局函数
-		var fnName = 'getData' + parseInt(Math.random()*1000000000);
-		// 对象的属性是变量,需要用[]
-		window[fnName] = function(data){
-			opt.callback(data);
-			//执行完行数清除请求数据时创建的script标签
-			document.head.removeChild(script);
-			//同时清除当前的全局函数
-			delete window[fnName];
-		}
+        // 1.声明一个全局函数
+        var fnName = 'getData' + parseInt(Math.random() * 1000000000);
+        // 对象的属性是变量,需要用[]
+        window[fnName] = function(data) {
+            opt.callback(data);
+            //执行完行数清除请求数据时创建的script标签
+            document.head.removeChild(script);
+            //同时清除当前的全局函数
+            delete window[fnName];
+        };
 
-		//判断url中是否存在"?";
-		if(opt.url.indexOf("?") == -1){
-			opt.url += '?callback='+fnName;
-		}else{
-			opt.url += '&callback='+fnName;
-		}
-		script.src = opt.url;
-		//把script写入页面
-		document.head.appendChild(script);
-		//进入JSONP就不再执行下面的代码
-		return;
-	}
+        //判断url中是否存在"?";
+        if (opt.url.indexOf("?") == -1) {
+            opt.url += '?callback=' + fnName;
+        } else {
+            opt.url += '&callback=' + fnName;
+        }
+        script.src = opt.url;
+        //把script写入页面
+        document.head.appendChild(script);
+        //进入JSONP就不再执行下面的代码
+        return;
+    }
 
-	// ajax 请求
-	var xhr;
-	//处理兼容性
-	try {
-		// 尝试执行这里的代码
-		xhr = new XMLHttpRequest();
-	} catch (error) {
-		// 如果有错误，执行这里的代码，并把错误信息保存在error中
-		// 并不会在浏览器中抛出错误，也不会中断代码的执行
-		try {
-			xhr = new ActiveXObject("Msxml2.XMLHTTP");
-		} catch (error) {
-			try {
-				xhr = new ActiveXObject("Microsoft.XMLHTTP");
-			} catch (error) {
-				alert('你的世界我不懂~');
-			}
-		}
-	}
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState === 4 && xhr.status === 200) {
-			//对象调用方法
-			//&& 短路操作
-			typeof opt.callback === 'function' && opt.callback(JSON.parse(xhr.responseText));
-		}
-	}
-	xhr.open(opt.type, opt.url, opt.async);
-	xhr.send();
+    // ajax 请求
+    var xhr;
+    //处理兼容性
+    try {
+        // 尝试执行这里的代码
+        xhr = new XMLHttpRequest();
+    } catch (error) {
+        // 如果有错误，执行这里的代码，并把错误信息保存在error中
+        // 并不会在浏览器中抛出错误，也不会中断代码的执行
+        try {
+            xhr = new ActiveXObject("Msxml2.XMLHTTP");
+        } catch (error) {
+            try {
+                xhr = new ActiveXObject("Microsoft.XMLHTTP");
+            } catch (error) {
+                alert('你的世界我不懂~');
+            }
+        }
+    }
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            //对象调用方法
+            //&& 短路操作
+            //typeof opt.callback === 'function' && opt.callback(JSON.parse(xhr.responseText));
+            if (typeof opt.callback === 'function') {
+              opt.callback(JSON.parse(xhr.responseText));
+            }
+        }
+    };
+    xhr.open(opt.type, opt.url, opt.async);
+    xhr.send();
 
 }
 
@@ -376,52 +359,53 @@ function ajax(opt) {
 
 */
 //存入字符串
-function repet(str){
-	var arr = str.split('');
-	arr.sort();
-	//声明一个空的对象
-	var obj ={};
-	for (var i=0; i<arr.length;i++){
-		//判断obj中是否已经存在当前属性
-		if(obj[arr[i]] === undefined){
-			obj[arr[i]]=1;
-		}else{
-			obj[arr[i]]++;
-		}
-	}
+function repet(str) {
+    var arr = str.split('');
+    arr.sort();
+    //声明一个空的对象
+    var obj = {};
+    for (var i = 0; i < arr.length; i++) {
+        //判断obj中是否已经存在当前属性
+        if (obj[arr[i]] === undefined) {
+            obj[arr[i]] = 1;
+        } else {
+            obj[arr[i]]++;
+        }
+    }
 
-	//遍历对象,然后输出结果
-	var str_qc = "";
-	var str_res ="";
-	for (var key in obj){
-		str_qc += key;
-		str_res += key +":" + obj[key];
-		}
-//			console.log(str_res);
-			return str_qc;
+    //遍历对象,然后输出结果
+    var str_qc = "";
+    var str_res = "";
+    for (var key in obj) {
+        str_qc += key;
+        str_res += key + ":" + obj[key];
+    }
+    //			console.log(str_res);
+    return str_qc;
 }
-function repet2(arr){
-	arr.sort();
-	//声明一个空的对象
-	var obj ={};
-	for (var i=0; i<arr.length;i++){
-		//判断obj中是否已经存在当前属性
-		if(obj[arr[i]] === undefined){
-			obj[arr[i]]=1;
-		}else{
-			obj[arr[i]]++;
-		}
-	}
 
-	//遍历对象,然后输出结果
-	var str_qc = "";
-	var str_res ="";
-	for (var key in obj){
-		str_qc += key;
-		str_res += key +":" + obj[key];
-		}
-//			console.log(str_res);
-			return str_qc;
+function repet2(arr) {
+    arr.sort();
+    //声明一个空的对象
+    var obj = {};
+    for (var i = 0; i < arr.length; i++) {
+        //判断obj中是否已经存在当前属性
+        if (obj[arr[i]] === undefined) {
+            obj[arr[i]] = 1;
+        } else {
+            obj[arr[i]]++;
+        }
+    }
+
+    //遍历对象,然后输出结果
+    var str_qc = "";
+    var str_res = "";
+    for (var key in obj) {
+        str_qc += key;
+        str_res += key + ":" + obj[key];
+    }
+    //			console.log(str_res);
+    return str_qc;
 }
 
 // 获取页面元素
@@ -433,27 +417,71 @@ function repet2(arr){
  * @param {[type]} selector [description]
  * @param {[type]} context  [description]
  */
-function Element(selector,context){
-	var res;
-	var context = context || document;
-	if (document.querySelectorAll) {
-		res = context.querySelectorAll(selector);
-	} else {
-		//id选择器context
-		if(/^#/.test(selector)){
-			res = context.getElementById(selector.slice(1));
-		}
-		//类选择器
-		else if(/^\./.test(selector)){
-			res = context.getElementsByClassName(selector.slice(1));
-		}
-		//标签
-		else{
-			res = context.getElementsByTagName(selector);
-		}
-	}
-	if(res.length === 1){
-		res = res[0];
-	}
-	return res;
+function Element(selector, context) {
+    this.init(selector, context);
+}
+Element.prototype.init = function(selector, context) {
+    var res;
+    context = context || document;
+    if (document.querySelectorAll) {
+        res = context.querySelectorAll(selector);
+    } else {
+        //id选择器context
+        if (/^#/.test(selector)) {
+            res = context.getElementById(selector.slice(1));
+        }
+        //类选择器
+        else if (/^\./.test(selector)) {
+            res = context.getElementsByClassName(selector.slice(1));
+        }
+        //标签
+        else {
+            res = context.getElementsByTagName(selector);
+        }
+    }
+    if (res.length === 1) {
+        res = res[0];
+    }
+    this.ele = res;
+    return this;
+};
+Element.prototype.css = function(attr, val) {
+    var self = this;
+    //取值
+    if (typeof attr === "string" && val === undefined) {
+        return getStyle(this.ele, attr);
+    } else {
+        //赋值
+        if (typeof attr === 'object') {
+            for (var key in attr) {
+                setStyle(key, attr[key]);
+            }
+        } else {
+            setStyle(attr, val);
+        }
+    }
+    function setStyle(_attr, _val) {
+      if (self.ele.length === undefined) {
+        self.ele.style[_attr] = _val;
+      } else {
+        for (var i = 0; i < self.ele.length; i++) {
+          self.ele[i].style[_attr] = _val;
+        }
+      }
+    }
+    return this;
+};
+Element.prototype.bind = function(type, fn) {
+    if (this.ele.length) {
+        for (var i = 0; i < this.ele.length; i++) {
+            this.ele[i]['on' + type] = fn;
+        }
+    } else {
+        this.ele['on' + type] = fn;
+    }
+    return this;
+};
+
+function $(selector) {
+    return new Element(selector);
 }
